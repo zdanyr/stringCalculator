024b7f46feaa62fc7cb762273a25d4f24c365ce4
Daniela Rodriguez <zdanyr@gmail.com>
2020-02-11 08:57:23 +1300

    added test cases for summing string of numbers

--- add.js
+++ add.js
@@ -1,5 +1,13 @@
+
+
 function add(input) {
-    return 0
+    if(input ==='') return 0;
+let numbers = input.split(',');
+let sum = 0;
+numbers.forEach(element => {
+    sum=sum+parseInt(element);
+});
+   return sum;
 }
 
 module.exports = add;
--- add.test.js
+++ add.test.js
@@ -4,3 +4,14 @@ test('Empty string should return zero ', () => {
     const result = add("");
     expect(result).toBe(0);
 });
+
+test('The single number should return the input number ', () => {
+    const result = add("1");
+    expect(result).toBe(1);
+});
+
+test('Comma separator numbers should return sum ', () => {
+    const result = add("1,2")
+    expect(result).toBe(3);
+    
+});
