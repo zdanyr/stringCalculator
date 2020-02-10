c6877b7379e292902c2c1b8c147b3dd42bb5f902
Daniela Rodriguez <zdanyr@gmail.com>
2020-02-11 09:15:07 +1300

    refactor test

--- add.test.js
+++ add.test.js
@@ -5,19 +5,9 @@ test('Empty string should return zero ', () => {
     expect(result).toBe(0);
 });
 
-test('The single number should return the input number ', () => {
-    const result = add("1");
-    expect(result).toBe(1);
-});
-
-test('Comma separator numbers should return sum ', () => {
-    const result = add("1,2")
-    expect(result).toBe(3);
-
-});
-
 test.each`
    input   | expected
+   ${"1"} | ${1}
    ${"1,2"} | ${3}
    ${"1,2,3"} | ${6}
 `('returns $expected when $input is added', ({input, expected}) => {
