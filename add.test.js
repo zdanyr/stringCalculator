9a76134ff2639d10d17088855d3a72242a656190
Daniela Rodriguez <zdanyr@gmail.com>
2020-02-12 12:56:23 +1300

    added test case to test inputToArray

--- add.test.js
+++ add.test.js
@@ -27,14 +27,19 @@ test.each`
   expect(result).toBe(expected);
 });
 
+test.each`
+input | expected
+${'1,2,3'} | ${['1','2','3']}
+${'3\n5\n30,9'} | ${['3','5','30','9']}
+`('Comma separator or break line separator should return array',({input, expected})=>{
+    const result = inputToArray(input);
+    expect(result).toEqual(expected);
+});
+
 test("should return sum of elements", () => {
   const toSum = [1, 2, 3, 4];
   const result = sum(toSum);
   expect(result).toBe(10);
 });
 
-test('comma separator input should transform into array',()=>{
-const string = '1,2,3';
-const result = inputToArray(string);
-expect(result).toEqual(expect.arrayContaining(result));
-})
+
