37e933999fa159b7b0836e62a51e81ef1e8df83d
Daniela Rodriguez <zdanyr@gmail.com>
2020-02-18 13:44:39 +1300

    refactor add

--- add.js
+++ add.js
@@ -4,12 +4,11 @@ function add(input) {
   const arrayOfNumbers = cleanUpInput(input);
   return sum(arrayOfNumbers);
 }
+
 function sum(numbers) {
   let sum = 0;
+  if (numbers === 0) return 0;
 
-  if (numbers === 0) {
-    return 0;
-  }
   numbers.forEach(element => {
     sum = sum + parseInt(element);
   });
