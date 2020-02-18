2373f2347d4e4657538b8fc03d0110ccbfe70a16
Daniela Rodriguez <zdanyr@gmail.com>
2020-02-18 13:51:46 +1300

    added test for isComaOrBreakLineSeparator

--- utils.test.js
+++ utils.test.js
@@ -1,5 +1,6 @@
 const {
   isCustomSeparator,
+  isCommaOrBreakLineSeparator,
   isOneOrManyCustomDelimitersAnyLength,
 } = require('./utils');
 
@@ -17,8 +18,25 @@ test.each`
 );
 
 test.each`
+  input            | expected
+  ${'3,5,3,9'}     | ${true}
+  ${'1,2\n3'}      | ${true}
+  ${'3\n5\n30,9'}  | ${true}
+  ${'3\n5\n,30,9'} | ${false}
+`(
+  'isCommaOrBreakLineSeparator should return $expected when $input',
+  ({ input, expected }) => {
+    const result = isCommaOrBreakLineSeparator(input);
+    expect(result).toBe(expected);
+  },
+);
+
+test.each`
   input                            | expected
   ${'//[a][b][c][d]\n1a2a3b3c4d5'} | ${true}
+  ${'//[***]\n1***2***3}'}         | ${true}
+  ${'//[***][#][%]\n10***2#3%4'}   | ${true}
+  ${'//[**1*][%%]\n1**1*2%%3'}     | ${true}
   ${'//-\n1-2-9'}                  | ${false}
 `(
   'isOneOrManyCustomDelimitersAnyLength should return $expected when $input',
